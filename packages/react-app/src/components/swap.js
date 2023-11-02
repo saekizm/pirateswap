27e800b8b34bea01f22ede016c7ff3ad7ec6dd18
saekizm <saekizm@proton.me>
2023-11-02 01:35:44 +0000

    cro support for swap

--- packages/react-app/src/components/swap.js
+++ packages/react-app/src/components/swap.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { useEthers, useTokenBalance, useContractFunction } from '@usedapp/core';
+import { useEthers, useTokenBalance, useContractFunction, useEtherBalance } from '@usedapp/core';
 import { Contract } from '@ethersproject/contracts';
 import { formatUnits, parseUnits } from '@ethersproject/units';
 import { Button, Grid, Typography, Container } from '@mui/material';
@@ -7,6 +7,7 @@ import { MAINNET_ID, addresses, abis } from "./contracts";
 import TokenInput from "./TokenInput"
 import { constants } from 'ethers';
 import TokenDialog from './TokenDialog';
+import useBalance from './useBalance';
 
 const Swap = () => {
   const [tokenA, setTokenA] = useState('');
@@ -18,28 +19,44 @@ const Swap = () => {
   const [approvalA, setApprovalA] = useState(false);
   const [approvalB, setApprovalB] = useState(false);
   const { library: provider, account } = useEthers();
+  const isETH = (tokenAddress) => tokenAddress === '';
 
-  const tokenContractA = useMemo(() => tokenA ? new Contract(tokenA, abis.erc20.abi, provider.getSigner()) : null, [tokenA, provider]);
-  const tokenContractB = useMemo(() => tokenB ? new Contract(tokenB, abis.erc20.abi, provider.getSigner()) : null, [tokenB, provider]);
+  const tokenContractA = useMemo(() => {
+    return !isETH(tokenA) ? new Contract(tokenA, abis.erc20.abi, provider.getSigner()) : null;
+  }, [tokenA, provider]);
 
-  const tokenABalance = useTokenBalance(tokenA, account);
-  const tokenBBalance = useTokenBalance(tokenB, account);
+  const tokenContractB = useMemo(() => {
+    return !isETH(tokenB) ? new Contract(tokenB, abis.erc20.abi, provider.getSigner()) : null;
+  }, [tokenB, provider]);
+  const tokenABalance = useBalance(tokenA, account);
+  const tokenBBalance = useBalance(tokenB, account);
+
   const [decimalsA, setDecimalsA] = useState(null);
   const [decimalsB, setDecimalsB] = useState(null);
   const [symbolA, setSymbolA] = useState(null)
   const [symbolB, setSymbolB] = useState(null)
+  const etherBalance = useEtherBalance(account);
+
+  const DEFAULT_DECIMALS = 18;
+
+  const effectiveTokenA = isETH(tokenA) ? addresses[MAINNET_ID].tokens.WCRO : tokenA;
+  const effectiveTokenB = isETH(tokenB) ? addresses[MAINNET_ID].tokens.WCRO : tokenB;
 
   // Handler for maximum balance
   const handlePercentage = async (field, percentage) => {
     const balance = field === 'A' ? tokenABalance : tokenBBalance;
-    const decimals = field === 'A' ? decimalsA : decimalsB;
-    if (balance && decimals) {
+    // Check if the token is ETH to decide which decimals to use
+    const decimals = field === 'A' ? (isETH(tokenA) ? DEFAULT_DECIMALS : decimalsA) : (isETH(tokenB) ? DEFAULT_DECIMALS : decimalsB);
+
+    if (balance && decimals != null) {
       const formattedBalance = formatUnits(balance.mul(percentage).div(100), decimals);
       if (field === 'A') {
         setAmountA(formattedBalance);
+        console.log(formattedBalance)
       } else {
         setAmountB(formattedBalance);
       }
+      console.log(formatUnits(balance, decimals), formattedBalance);
     }
   };
 
@@ -63,26 +80,64 @@ const Swap = () => {
 
   const uniswapV2RouterContract = new Contract(addresses[MAINNET_ID].router02, abis.router02);
 
-  const { send: swapTokens, state: swapState } = useContractFunction(uniswapV2RouterContract, 'swapExactTokensForTokens', {
-    transactionName: 'Swap Tokens',
+  const { send: swapExactETHForTokens } = useContractFunction(uniswapV2RouterContract, 'swapExactETHForTokens', {
+    transactionName: 'Swap ETH for Tokens',
   });
+  
+  const { send: swapExactTokensForETH } = useContractFunction(uniswapV2RouterContract, 'swapExactTokensForETH', {
+    transactionName: 'Swap Tokens for ETH',
+  });
+  
+  const { send: swapExactTokensForTokens, state: swapTokensState } = useContractFunction(uniswapV2RouterContract, 'swapExactTokensForTokens', {
+    transactionName: 'Swap Tokens for Tokens',
+  });
+  
 
   const handleSwap = async () => {
-    if (!tokenContractA.decimals || !tokenContractB.decimals) {
-      console.error("Token decimals not available");
-      return;
+    // Define WCRO address for the network (assuming this is your wrapped ETH equivalent)
+    const WCRO = addresses[MAINNET_ID].tokens.WCRO;
+    
+    // Check if token A or B is ETH and use the WCRO address in the path
+    const path = [
+      isETH(tokenA) ? WCRO : tokenA,
+      isETH(tokenB) ? WCRO : tokenB,
+    ];
+    
+    if (isETH(tokenA)) {
+      // If token A is ETH, use swapExactETHForTokens
+      const parsedAmountBMin = parseUnits(amountB, decimalsB); // Minimum amount of token B to accept
+      await swapExactETHForTokens(
+        parsedAmountBMin,  // Min amount of tokens B to accept (with slippage)
+        path,              // Path: [WCRO, tokenB]
+        account,
+        Math.floor(Date.now() / 1000) + 60 * 20,  // Deadline
+        { value: parseUnits(amountA, 'ether') }   // Amount of ETH to send
+      );
+    } else if (isETH(tokenB)) {
+      // If token B is ETH, use swapExactTokensForETH
+      const parsedAmountA = parseUnits(amountA, decimalsA); // Amount of token A to swap
+      await swapExactTokensForETH(
+        parsedAmountA,
+        0,  // Min amount of ETH to accept (with slippage)
+        path,              // Path: [tokenA, WCRO]
+        account,
+        Math.floor(Date.now() / 1000) + 60 * 20  // Deadline
+      );
+    } else {
+      // If neither token is ETH, use swapExactTokensForTokens
+      const parsedAmountA = parseUnits(amountA, decimalsA); // Amount of token A to swap
+      await swapExactTokensForTokens(
+        parsedAmountA,
+        0,  // Min amount of token B to accept (with slippage)
+        path,              // Path: [tokenA, tokenB]
+        account,
+        Math.floor(Date.now() / 1000) + 60 * 20  // Deadline
+      );
     }
-
-    const parsedAmountA = parseUnits(amountA, decimalsA);
-
-    await swapTokens(
-      parsedAmountA,
-      0,  // minimum amount of tokens B to accept
-      [tokenA, tokenB],  // path
-      account,
-      Math.floor(Date.now() / 1000) + 60 * 20  // deadline: 20 minutes from the current Unix time
-    );
   };
+  
+  
+  
 
   const handleTokenSelect = (token) => {
     // handle the token selection
@@ -132,11 +187,14 @@ const Swap = () => {
 
   useEffect(() => {
     const fetchExpectedAmountB = async () => {
-      if (amountA && decimalsA && tokenA && tokenB && provider) {  // Check if provider is defined
-        const parsedAmountA = parseUnits(amountA, decimalsA);
+      const decimalsForTokenA = isETH(tokenA) ? DEFAULT_DECIMALS : decimalsA;
+      const decimalsForTokenB = isETH(tokenB) ? DEFAULT_DECIMALS : decimalsB;
+      console.log("a: ", amountA, decimalsForTokenA, tokenB);
+      if (amountA && decimalsForTokenA) {  // Check if provider is defined
+        const parsedAmountA = parseUnits(amountA, decimalsForTokenA);
         const uniswapV2RouterContract = new Contract(addresses[MAINNET_ID].router02, abis.router02, provider.getSigner());
         try {
-          const amountsOut = await uniswapV2RouterContract.getAmountsOut(parsedAmountA, [tokenA, tokenB]);
+          const amountsOut = await uniswapV2RouterContract.getAmountsOut(parsedAmountA, [effectiveTokenA, effectiveTokenB]);
           const expectedAmountB = formatUnits(amountsOut[1], decimalsB);
           setAmountB(expectedAmountB);
 
@@ -205,8 +263,8 @@ const Swap = () => {
                 if (approvalA) handleApprove(tokenA, tokenContractA, setApprovalA);
                 if (approvalB) handleApprove(tokenB, tokenContractB, setApprovalB);
               }}
-              disabled={!account || swapState.status === 'Mining'}
-            >
+              disabled={!account || swapTokensState.status === 'Mining' || approvalA || approvalB}
+              >
               `Approve ${approvalA ? symbolA : ''} ${approvalB ? symbolB : ''}`
             </Button>
           )) || (
@@ -215,8 +273,8 @@ const Swap = () => {
                 color="primary"
                 fullWidth
                 onClick={handleSwap}
-                disabled={!account || swapState.status === 'Mining' || approvalA || approvalB}
-              >
+                disabled={!account || swapTokensState.status === 'Mining' || approvalA || approvalB}
+                              >
                 Swap
               </Button>
             )}
